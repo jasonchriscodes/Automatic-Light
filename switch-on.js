41882751b72fe4de1f6e9f36dc7acc7fa4dd4a41
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-12 16:28:26 +1300

    chore: add led on/off logic

---
+++ switch-on.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const Gpio = require('onoff').Gpio;
+const LED = new Gpio(4, 'out'); // gpio 4 as out
+
+LED.writeSync(1); // making the gpio 4 on. Will turn LED on
+
+
+function switchOff(){
+  LED.writeSync(0); // making the gpio 4 off. Will turn LED off
+  LED.unexport(); // Unexport GPIO to free resources
+}
+
+setTimeout(switchOff, 10000);
